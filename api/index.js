89102c78a58e583d56d42e23bd6b7e1450ca781b
Pedro <pedrohanrri1@gmail.com>
2026-05-18 01:46:33 -0300

    oi

--- api/index.js
+++ api/index.js
@@ -142,7 +142,7 @@ app.get('/api/imoveis/:id', async (req, res) => {
         const [rows] = await pool.query(`
             SELECT i.*, u.nome AS proprietario, t.descricao AS tipo
             FROM tbImovel i
-            JOIN seguranca.tbUsuarios u ON u.usuario_id = i.proprietario_id
+            JOIN seguranca_tbUsuarios u ON u.usuario_id = i.proprietario_id
             JOIN tbImovelTipo t         ON t.imovel_tipo_id = i.imovel_tipo_id
             WHERE i.imovel_id = ?
         `, [req.params.id]);
